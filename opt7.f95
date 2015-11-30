bfbfec4f11c4d37c8ec7ced6bcb9c69c75f947ba
Hayden Martz <hmartz@crimson.ua.edu>
2015-11-29 22:53:41 -0600

    Hacked together option 7

--- opt7.f95
+++ opt7.f95
@@ -1,7 +1,29 @@
 SUBROUTINE opt7
-  CALL SYSTEM("clear")
-  PRINT *, "RUN OPTION 7 SUBROUTINE"
-  WRITE (*,'(a)', ADVANCE='NO') "Press Enter to Continue: "
-  READ*, !Put in subroutine option later.
+
+  IMPLICIT NONE
+  INTEGER :: Lengthdb, I
+  CHARACTER :: Recorded*105
+  WRITE (*,100) "Police Information System"
+100 FORMAT(T30,a)
+  WRITE (*,150) "Listing Master Database"
+150 FORMAT(T31,a,//)
+ 
+  OPEN(12, FILE = "master.db", FORM = "FORMATTED", ACCESS = "DIRECT", RECL = 105)
+  READ(12, "(i2)", REC = 1) Lengthdb
+
+  DO I=2, Lengthdb
+    READ(12, "(a105)", REC = I) Recorded
+    WRITE(*, "(T20, a105)") Recorded
+    IF(MOD((I-1),25) == 0) THEN
+      WRITE(*, 100, ADVANCE = "NO") "Press Enter to View the next 25: "
+      READ*,
+      CALL SYSTEM("clear")
+      WRITE(*, 100) "Police Information System"
+      WRITE(*, 150) "Listing Master Database"
+    END IF
+  END DO
+
+  WRITE(*, "(T25, a)", ADVANCE = "NO") "Press enter to return to main menu: "
+  READ*, 
 
 END SUBROUTINE opt7
